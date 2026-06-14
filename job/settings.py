a4195c5dffd845fc9bf3cd846cf0de8c9b528d3e
kathier02 <kathierrk03@gmail.com>
2026-06-14 12:30:58 +0530

    Remove media settings from job/settings.py
    Removed MEDIA_URL and MEDIA_ROOT settings.

--- job/settings.py
+++ job/settings.py
@@ -134,8 +134,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email (Gmail SMTP)
