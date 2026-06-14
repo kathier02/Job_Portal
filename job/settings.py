bb9487ebe1c21761526f1951049d0535a79e4b39
kathier02 <kathierrk03@gmail.com>
2026-06-14 12:25:40 +0530

    Fix syntax error in settings.py

--- job/settings.py
+++ job/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job_portal'.
+    'job_portal',
     'cloudinary',
     'cloudinary_storage',
 ]
