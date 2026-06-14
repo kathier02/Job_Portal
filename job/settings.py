16baaca138c612823fcc972f18375efa53962dff
kathier02 <kathierrk03@gmail.com>
2026-06-14 12:35:56 +0530

    Configure Cloudinary in settings.py
    Added Cloudinary configuration to settings.

--- job/settings.py
+++ job/settings.py
@@ -12,7 +12,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
 
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
